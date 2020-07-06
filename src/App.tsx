06bf90177b4e20b86010904bafa7b4664a980392
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-06 12:06:02 +0200

    passed some dummy props values

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <PokemonSearch />
+      <PokemonSearch name='John Doe' numberOfPokemons={5}/>
     </div>
   );
 }
