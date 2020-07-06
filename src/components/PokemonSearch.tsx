c4c8e9d1c049dd1f6573d13d1280b7b67a66cc58
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-06 12:07:32 +0200

    extracted name and numberOfPokemons from props

--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 
 export class PokemonSearch extends Component<{name: string, numberOfPokemons: number}> {
   render() {
+    const {name, numberOfPokemons} = this.props
     return (
       <div>
-
       </div>
     )
   }
