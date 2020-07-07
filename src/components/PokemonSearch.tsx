4b9e780d20ce3f9bd0094e6dced14828735430de
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-07 16:52:48 +0200

    show value depending if it exists

--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import User from '../interfaces/User.interface'
+import { spawn } from 'child_process'
 
 export class PokemonSearch extends Component<User> {
   render() {
     const {name, numberOfPokemons} = this.props
     return (
       <div>
-        <p>User {name} has {numberOfPokemons} pokemons</p>
+        User {name} {numberOfPokemons && <span>has {numberOfPokemons} pokemons</span>}
       </div>
     )
   }
